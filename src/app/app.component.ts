d1c753dd578ab1c1f4345e903a727bc6f1fd6f5a
mh453Uol <mh453@student.le.ac.uk>
2019-03-04 07:20:57 +0000

    ng-content and contentchild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { PanelComponent } from './panel/panel.component';
+import {
+  Component,
+  ContentChild,
+  AfterContentInit,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
   title = 'app';
+
+  @ContentChild('panel') panel: ElementRef;
+
+  ngAfterContentInit(): void {
+    console.log(this.panel);
+  }
+
+  onClick() {
+    console.log(this.panel);
+  }
 }
